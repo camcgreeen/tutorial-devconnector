b8d76ab84b2d4c12480cfdc4bcc5a6c254dc87f4
Cam Green <c.c.green@outlook.com>
2021-02-22 11:12:53 +0000

    add user registration functionality to /api/users endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
 
 // @route  POST api/users
 // @desc   Register user
@@ -15,14 +19,52 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // send a bad request response if input not valid
       // this is checked by express-validator
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('user route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if the user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get user's gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password using bcrypt (salt and hash)
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      // HTTP status code 500 means internal server error
+      res.status(500).send('Server error');
+    }
   }
 );
 
