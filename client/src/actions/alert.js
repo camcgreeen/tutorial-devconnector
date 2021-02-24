685d7f9592b74504022dcb1a2c57a742a3682020
Cam Green <c.c.green@outlook.com>
2021-02-24 08:16:16 +0000

    set up alert system

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,12 +1,14 @@
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 // thunk middleware allows this
-export const setAlert = (msg, alertType) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
   // uuid package generates random universal id
-  const id = uuid.v4();
+  const id = uuidv4();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id },
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
