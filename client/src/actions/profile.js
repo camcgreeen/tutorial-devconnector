aa3b72ff7871b7e64ddb35c757ce29ec8562ade9
Cam Green <c.c.green@outlook.com>
2021-02-24 14:22:38 +0000

    display GitHub repos on Profile component

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -61,7 +61,7 @@ export const getProfileById = (userId) => async (dispatch) => {
 };
 
 // Get GitHub repos
-export const getGitHubRepos = (username) => async (dispatch) => {
+export const getGithubRepos = (username) => async (dispatch) => {
   try {
     const res = await axios.get(`/api/profile/github/${username}`);
     dispatch({
